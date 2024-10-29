7e67b70250dca7cbfad250448ba7425a677e3ba5
KirllBashorin <99118225+KirllBashorin@users.noreply.github.com>
2024-10-30 00:42:32 +0400

    Responsive scal

--- src/js/main.js
+++ src/js/main.js
@@ -5,23 +5,31 @@ import Transition from './scenes/transition.js';
 import Game from './scenes/game.js';
 import Outro from './scenes/outro.js';
 
-import '../style.css'
+import '../style.css';
 
 const gameConfig = {
     type: Phaser.AUTO,
-    width: 360,
-    height: 700,
     scale: {
         mode: Phaser.Scale.FIT,
+        parent: 'app',
+        width: window.innerWidth,
+        height: window.innerHeight,
+        max: {
+            width: 768,
+            height: '100%',
+        }
     },
-    parent: 'app',
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: {x: 100, y: 200}
+            gravity: { x: 100, y: 200 }
         }
     },
     scene: [Bootloader, Splash, Transition, Game, Outro],
 };
 
 const game = new Phaser.Game(gameConfig);
+
+window.addEventListener('resize', () => {
+    game.scale.resize(window.innerWidth, window.innerHeight);
+});
